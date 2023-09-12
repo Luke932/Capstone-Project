1bd1f5151f33141fd7d83db808a4363e000ebf91
Luke932 <126018318+Luke932@users.noreply.github.com>
2023-09-12 12:00:56 +0200

    modifiche

--- StreetFood_FE/src/app/app.component.ts
+++ StreetFood_FE/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RoleService } from './auth/role.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+isLoading = false;
+userRole!: string;
+
+constructor (private roleService: RoleService) {}
+
+
+ngOnInit(): void {
+  const userRole = this.roleService.getUserRole();
+  }
+
   title = 'StreetFood_FE';
 }
